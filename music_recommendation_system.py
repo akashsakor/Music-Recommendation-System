66af6e5f6d79fbc3d630283044646c27fb4ddee9
akash <akashsa.info@gmail.com>
2023-11-23 22:51:13 +0530

    first commit

---
+++ music_recommendation_system.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import os
+from sklearn.cluster import KMeans
+from sklearn.metrics import euclidean_distances
+from sklearn.pipeline import Pipeline
+import warnings
+warnings.filterwarnings("ignore")
+
